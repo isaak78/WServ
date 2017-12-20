e27439c3f21176a4410189ed8db4e0885487ad78
isaak78 <isaak.ccn@gmail.com>
2017-12-20 18:56:51 +0000

    Update autoaz.sh

--- autoaz.sh
+++ autoaz.sh
@@ -6,7 +6,7 @@ echo "starting"
 
 sudo su
 sudo apt update && apt-get install automake autoconf pkg-config libjansson-dev libssl-dev libgmp-dev make g++ git -y
-sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config automake yasm -y
+sudo apt-get install libcurl4-openssl-dev libncurses5-dev pkg-config yasm -y
 
 git clone https://github.com/OhGodAPet/cpuminer-multi.git
 cd cpuminer-multi
